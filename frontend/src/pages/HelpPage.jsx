03bf113c11afa41b538391774bf901cf1be516fd
hughjsl <124052728+hughjsl@users.noreply.github.com>
2025-02-19 06:55:07 +0000

    Updated other pages to follow homepage style

--- frontend/src/pages/HelpPage.jsx
+++ frontend/src/pages/HelpPage.jsx
@@ -1,7 +1,8 @@
 export default function HomePage() {
     return (
-        <div> 
-            <h1>Help Page</h1>
-        </div>
-    )
-}
+      <div className="w-full h-[8%] bg-gray-100 border-b-2 border-gray-300 flex items-center px-6 shadow-lg z-0 relative">
+        <h1 className="text-3xl font-bold text-gray-600 px-6">Help</h1>
+      </div>
+    );
+  }
+  
